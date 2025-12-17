b92c46bcbe5dbe87a746c0009330ea3e2ead9c1b
bala-amypo <bala@amypo.in>
2025-12-17 08:15:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/studententity.java
+++ src/main/java/com/example/demo/entity/studententity.java
@@ -1,60 +1,63 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
 
 @Entity
-public class studententity{
+public class StudentEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
     private String password;
-    private Date created;    
+    private Date created;
+ 
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 
+    public Integer getId(){
+        return id;
+    }
+    public void setId(Integer id){
+        this.id=id;
+    }
 
-public Integer getId(){
-    return id;
-}
-public void setId(Integer id){
-    this.id=id;
-}
-public String getName{
-    return name;
-}
-public void setName(String name){
-    this.name=name;
-}
-public String getEmail{
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
-}
-public String getPassword(){
-    return password;
-}
-public void setPassword(String password){
-    this.password=password;
-}
-public Date getCreated(){
-    return created;
-}
-public void setCreated(Date created){
-    this.created=created;
-}
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public Date getCreated(){
+        return created;
+    }
+    public void setCreated(Date created){
+        this.created=created;
+    }
 
-
-public studententity(Integer id,String name,String email,String password,Date created){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.password=password;
-    this.created=created;
-}
-
+  public StudentEntity(String name,Integer id,String email,String password,Date created){
+        this.name=name;
+        this.id=id;
+        this.email=email;
+        this.password=password;
+        this.created=created;
+    }
+       
+    public StudentEntity(){
+    }
 }
