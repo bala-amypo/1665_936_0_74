c6db919791864a30647e5ee60fc9a1d716d127c0
bala-amypo <bala@amypo.in>
2025-12-17 09:20:32 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -13,6 +13,6 @@ public class StudentServiceImpl implements StudentService{
 // existByid() 
 
    public studententity postData(studententity stu);
-    return student.save();
+    return student.save(stu);
 
 }
