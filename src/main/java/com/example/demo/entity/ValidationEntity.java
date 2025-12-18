4aa22095720a036b82e4b57933b46ff5960e8d34
bala-amypo <bala@amypo.in>
2025-12-18 09:25:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -96,4 +96,89 @@
 //     }
 // }
 
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+public class ValidationEntity{
+
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+@NotNull
+@Size(min=2,max=6,message="require 2 to 6 characters")
+private String name;
+@Email(message="Email is not valid")
+private String email;
+@Size(min=2,max=6,message="should be less than 6")
+@NotNull
+private String password;
+@Max(35)
+@Positive(message="Age cannot be negative")
+private int age;
+
+ public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+
+public ValidationEntity(Long id,
+@NotNull
+@Size(min=2,max=6,message="require 2 to 6 characters")
+String name,
+@Email(message="Email is not valid")
+ String email,
+@Size(min=2,max=6,message="should be less than 6")
+@NotNull
+ String password,
+@Max(35)
+@Positive(message="Age cannot be negative")
+int age){
+        this.name=name;
+        this.id=id;
+        this.email=email;
+        this.password=password;
+        this.age=age;
+    
+ }
+ public ValidationEntity(){
+
+ }
+}
 
--- src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
-import com.example.demo.service.ValidayionService;
+import com.example.demo.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.ValidationRepository;
 import com.example.demo.entity.ValidationEntity;
