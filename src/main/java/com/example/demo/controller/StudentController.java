f8e1e719065cf8b489b9afb7a6cc7911becd51c9
bala-amypo <bala@amypo.in>
2025-12-18 06:25:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -33,8 +34,8 @@ public class StudentController{
     }
 
     @GetMapping("/put/{id}")
-    public StudentEntity getdata(@PathVariable int id){
-        return ser.getdata(id);
+    public StudentEntity getData(@PathVariable int id){
+        return ser.getData(id);
     }
 
     @PutMapping("/put/{id}")
