1a9a609e28e3adc66a9f3fb54673f8ccfaa4fa58
bala-amypo <bala@amypo.in>
2025-12-18 08:57:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -3,6 +3,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
+
 public class ValidationController{
     @Autowired ValidationService ser;
     @PostMapping("/post")
---
+++ src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.entity.StudentEntity;
+
+
+@Service
+public class ValidationServiceimpl implements ValidationService{
+
+}
