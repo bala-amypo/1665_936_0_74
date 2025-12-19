078987191c11137d9f1467faab3d081be56e4e32
bala-amypo <bala@amypo.in>
2025-12-19 04:55:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
@@ -5,6 +5,7 @@ import com.example.demo.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.ValidationRepository;
 import com.example.demo.entity.ValidationEntity;
+import com.example.demo.exception.ValidationException;
 
 
 @Service
