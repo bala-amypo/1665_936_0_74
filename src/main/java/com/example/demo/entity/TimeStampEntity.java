705eede76062e7b57791061257688a043090c542
bala-amypo <bala@amypo.in>
2025-12-19 09:25:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -1,36 +1,34 @@
 package com.example.demo.entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-
-import jakarta.validation.constraints.*;
-import jakata.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
-import java.util.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import jakarta.persistance.PrePersist;
-import jakarta.persistance.updateAt
-
+import java.time.LocalDateTime;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 @Entity
 @Data
 @Getter
 @Setter
-@AllAgrsConstructor
-@NoAgrsConstructor
-@PrePersist
-@PreUpdate
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class TimeStampEntity{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY);
-    private Long id;
-    private String name;
-    private String email;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-}
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+     private Long id;
+     private String name;
+     private String email;
+     private LocalDateTime createAt;
+     private LocalDateTime updateAt;
+     @PrePersist
+     public void Oncreate(){
+        LocalDateTime now=LocalDateTime.now();
+        this.createAt=now;
+        this.updateAt=now;
+     }
+     @PreUpdate
+     public void Onupdate(){
+        LocalDateTime now=LocalDateTime.now();
+        this.updateAt=now;
+        }
+    }
